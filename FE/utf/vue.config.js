4cafc74e25bd19c82f1cb5cc2f7847cc1172edc0
kotaehee <koxogml@naver.com>
2022-08-01 22:51:01 +0900

    chore : gradle관련 plugin 추가

--- FE/utf/vue.config.js
+++ FE/utf/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  //npm run build 타겟 디렉토리
+  outputDir: "../../BE/utf/src/main/resources/static",
+
+  //포트 다르기때문에 프록시 설정
+  devServer: {
+    proxy: {
+      "/api": {
+        // '/api' 로 들어오면 포트 8081(스프링 서버)로 보낸다
+        target: "http://localhost:8081",
+        changeOrigin: true, // cross origin 허용
+      },
+    },
+  },
 });
