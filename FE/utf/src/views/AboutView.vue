cfd867ecaf75318a0f0035ed62c3546410bd7219
yangksks <yangksks@gmail.com>
2022-08-01 15:44:28 +0900

    test : 내부 Spring 구동 확인 테스트

--- FE/utf/src/views/AboutView.vue
+++ FE/utf/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a href="http://localhost:8081">하나둘삼넷</a>
   </div>
 </template>
--- FE/utf/src/views/HomeView.vue
+++ FE/utf/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="뷰로 도커 jenkins" />
   </div>
 </template>
 
