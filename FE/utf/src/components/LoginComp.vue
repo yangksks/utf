10649d579c768fc1d7b0d53f699eac18aef34c68
노호준 <hh950131@gmail.com>
2022-08-02 17:19:31 +0900

    feat/frontend : 카카오 로그인 기능만 구현

---
+++ FE/utf/src/components/LoginComp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <img
+      id="kakaoLoginBtn"
+      class="btn"
+      src="@/assets/image/kakao_login_medium_wide.png"
+      alt="카카오 로그인 버튼"
+    />
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+
+const REST_API_KEY = process.env.VUE_APP_REST_API_KEY;
+const REDIRECT_URI = process.env.VUE_APP_REDIRECT_URI;
+const getKakao = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+export default {
+  name: "LoginComp",
+  mounted() {
+    const kakaoLoginBtn = document.querySelector("#kakaoLoginBtn");
+    function kakaoLogin() {
+      window.location.assign(getKakao);
+      // axios
+      //   .get(getKakao)
+      //   .then(function (response) {
+      //     console.log(response);
+      //   })
+      //   .catch(function (error) {
+      //     console.log(error);
+      //   });
+    }
+    kakaoLoginBtn.onclick = kakaoLogin;
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  cursor: pointer;
+}
+</style>
---
+++ FE/utf/src/views/RedirectView.vue
@@ -0,0 +1,30 @@
+<template>redirect</template>
+
+<script>
+import axios from "axios";
+
+async function kakaoRedirect() {
+  const code = new URL(window.location.href).searchParams.get("code");
+  console.log(code);
+  if (code) {
+    await axios
+      .get(`http://localhost:8080/api/user/kakaoLogin?code=${code}`)
+      .then((res) => {
+        console.log("성공");
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log("에러");
+        console.log(err);
+      });
+    console.log("되나");
+  }
+}
+
+export default {
+  name: "RedirectView",
+  mounted() {
+    kakaoRedirect();
+  },
+};
+</script>
