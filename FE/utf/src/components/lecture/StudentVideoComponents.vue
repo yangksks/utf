154f38ffded179be2533ed17197a4dfab8b3b08b
yangksks <yangksks@gmail.com>
2022-08-05 21:30:59 +0900

    refactor : faceAPI 코드 병합

---
+++ FE/utf/src/components/lecture/StudentVideoComponents.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="video-components">
+    <div id="screen-share"></div>
+    <user-video
+      :stream-manager="publisher"
+      :class="{ highlight: speaker == 'me' }"
+    />
+    <user-video
+      v-for="sub in subscribers"
+      :key="sub.stream.streamId"
+      :stream-manager="sub"
+      :class="{ highlight: sub.stream.streamId == speaker }"
+    />
+  </div>
+</template>
+
+<script>
+import UserVideo from "@/components/video/UserVideo";
+
+export default {
+  name: "VideoComponents",
+
+  components: {
+    UserVideo,
+  },
+  props: {
+    publisher: Object,
+    subscribers: Object,
+    speaker: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+#video-components {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(4, calc(25% - 4px));
+  row-gap: 4px;
+}
+
+.highlight {
+  border: 2px;
+  border-style: solid;
+  border-color: blue;
+}
+</style>
