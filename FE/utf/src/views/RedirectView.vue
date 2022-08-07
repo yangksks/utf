f9f960e3f61776c69994ea4067d07a91d1fc51d9
노호준 <hh950131@gmail.com>
2022-08-07 17:03:18 +0900

    refactor/frontend : 코드 리팩토링

--- FE/utf/src/views/RedirectView.vue
+++ FE/utf/src/views/RedirectView.vue
@@ -1,84 +1,31 @@
 <template>redirect</template>
 
 <script>
-import axios from "axios";
-import store from "@/store/index.js";
-import router from "@/router/index.js";
+import { loginRequest } from "@/api/index.js";
 
-async function redirect() {
+function redirect() {
   const code = new URL(window.location.href).searchParams.get("code");
   const state = new URL(window.location.href).searchParams.get("state");
   const scope = new URL(window.location.href).searchParams.get("scope");
+  let socialLoginType = 0;
   //카카오 로그인
   if (code && !state && !scope) {
-    await axios
-      .post(`http://localhost:8080/api/user/kakaoLogin`, {
-        data: {
-          code: code,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        if (!res.data["userExist"]) {
-          store.dispatch("saveTempUserInfo", res.data);
-          router.push({ path: "/joinForm" });
-        } else {
-          store.dispatch("saveUserInfo", res.data);
-          router.push({ path: "/main" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        router.push({ path: "/login" });
-      });
+    socialLoginType = 1;
   }
   //네이버 로그인
   else if (code && state && !scope) {
-    await axios
-      .post(`http://localhost:8080/api/user/naverLogin`, {
-        data: {
-          code: code,
-          state: state,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        if (!res.data["userExist"]) {
-          store.dispatch("saveTempUserInfo", res.data);
-          router.push({ path: "/joinForm" });
-        } else {
-          store.dispatch("saveUserInfo", res.data);
-          router.push({ path: "/main" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        router.push({ path: "/login" });
-      });
+    socialLoginType = 2;
   }
   //구글 로그인
   else if (code && state && scope) {
-    await axios
-      .post(`http://localhost:8080/api/user/googleLogin`, {
-        data: {
-          code: code,
-        },
-      })
-      .then((res) => {
-        console.log(res);
-        if (!res.data["userExist"]) {
-          store.dispatch("saveTempUserInfo", res.data);
-          router.push({ path: "/joinForm" });
-        } else {
-          store.dispatch("saveUserInfo", res.data);
-          router.push({ path: "/main" });
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        router.push({ path: "/login" });
-      });
+    socialLoginType = 3;
   }
+  loginRequest(
+    `http://localhost:8080/api/user/socialLogin`,
+    socialLoginType,
+    code,
+    state
+  );
 }
 
 export default {
