d0aa9c041cab7a309449c89399b8d8e93b146af0
kotaehee <koxogml@naver.com>
2022-08-08 20:55:47 +0900

    feat/statistics : 통계 서버 개발

--- FE/utf/src/store/modules/StatisticsStore.js
+++ FE/utf/src/store/modules/StatisticsStore.js
@@ -19,7 +19,14 @@ const statisticsStore = {
     setEmotion: (store, item) => {
       //item: [이름,감정,점수]
       axios
-        .post(`http://localhost:8081/api/statistics/commit`, item)
+        .post(
+          `http://localhost:8081/api/statistics/commit`,
+          JSON.stringify({
+            name: item[0],
+            emotion: item[1],
+            score: item[2],
+          })
+        )
         .then((res) => {
           console.log(res);
         })
