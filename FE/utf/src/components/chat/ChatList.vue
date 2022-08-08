4d02c7a98ff5a4c7b546d2ea40958439732a695d
jaehoon <arza@naver.com>
2022-08-08 17:26:32 +0900

    feat/frontend : 채팅 디자인 및 자리비움 기능 완성

--- FE/utf/src/components/chat/ChatList.vue
+++ FE/utf/src/components/chat/ChatList.vue
@@ -26,8 +26,26 @@ export default {
   padding: 1rem;
   overflow: scroll;
   scroll-behavior: smooth;
+  background: #ffffff;
+  /* box-shadow: 0px 3px 10px rgba(0, 0, 0, 0.05); */
+  height: 500px;
+  border-radius: 12px 12px 0px 0px;
 }
-.chat-body::-webkit-scrollbar {
+/* .chat-body::-webkit-scrollbar {
   display: none;
+} */
+.chat-body::-webkit-scrollbar {
+  width: 6px;
+}
+.chat-body::-webkit-scrollbar-track {
+  background-color: transparent;
+}
+.chat-body::-webkit-scrollbar-thumb {
+  border-radius: 3px;
+  background-color: gray;
+}
+.chat-body::-webkit-scrollbar-button {
+  width: 0;
+  height: 0;
 }
 </style>
