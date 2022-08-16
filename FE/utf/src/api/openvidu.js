4cc1f0c68e9c75c5f180b49dd1d866842e79d4a5
yangksks <yangksks@gmail.com>
2022-08-16 16:56:18 +0900

    feat/frontend : 강의 통계 녹화영상 바인딩

--- FE/utf/src/api/openvidu.js
+++ FE/utf/src/api/openvidu.js
@@ -21,12 +21,13 @@ async function recordingStopApi(recordingId, success, fail) {
 }
 
 async function recordingStartApi(sessionId, success, fail) {
+  let fileName = Math.random().toString(36).substring(2, 11);
   await api
     .post(
       `/openvidu/api/recordings/start`,
       JSON.stringify({
         session: sessionId,
-        name: "testRecording",
+        name: fileName,
         hasAudio: true,
         hasVideo: true,
         outputMode: "COMPOSED",
