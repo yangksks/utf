934f939f08358004b84a81eb51dba27df11f0533
노호준 <hh950131@gmail.com>
2022-08-17 11:16:45 +0900

    fix/frontend : 강의실 추가버튼 크기조정, 프로필 배치 변경

--- FE/utf/src/components/main/Profile.vue
+++ FE/utf/src/components/main/Profile.vue
@@ -2,19 +2,24 @@
 <template>
   <b-container>
     <div :class="{ profileBox: !darkMode, profileBoxDark: darkMode }">
-      <b-row class="row">
+      <!-- <b-row class="row">
         <b-col sm="10" class="welcomeMsg">
           <div>{{ userName }} 강사님 환영합니다</div>
         </b-col>
         <b-col sm="2" class="btnGroup">
           <div class="settingBtnWrap">
-            <i class="bi bi-gear buttons" id="settingBtn" @click="updateProfile()"></i>
+            <i class="bi bi-gear buttons" id="settingBtn" style="font-size: 30px" @click="updateProfile()"></i>
           </div>
           <div class="logoutBtnWrap">
-            <i class="bi bi-box-arrow-right buttons" id="logoutBtn" @click="logout()"></i>
+            <i class="bi bi-box-arrow-right buttons" id="logoutBtn" style="font-size: 30px" @click="logout()"></i>
           </div>
         </b-col>
-      </b-row>
+      </b-row> -->
+      <div style="margin-top: 20px">{{ userName }} 강사님 환영합니다</div>
+      <div>
+        <i class="bi bi-gear buttons" id="settingBtn" style="font-size: 20px; margin-left: 30px; margin-right: 170px" @click="updateProfile()"></i>
+        <i class="bi bi-box-arrow-right buttons" id="logoutBtn" style="font-size: 20px" @click="logout()"></i>
+      </div>
     </div>
   </b-container>
 </template>
@@ -39,10 +44,10 @@ export default {
     },
     logout() {
       let flag = confirm("로그아웃 하시겠습니까??");
-      if(flag){
+      if (flag) {
         this.$emit("logout");
       }
-    }
+    },
   },
 };
 </script>
@@ -55,11 +60,5 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
-}
-.settingBtnWrap {
-  margin-top: 18px;
-}
-.logoutBtnWrap {
-  margin-top: 0px;
 }
 </style>
